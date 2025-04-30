b91f8e11bafd53ddea6e4051ad80c3943aa4e013
AdiY <YDdevelops@gmail.com>
2025-04-30 08:34:46 +0300

    some commits

--- about/src/components/fireFlies/firedots.jsx
+++ about/src/components/fireFlies/firedots.jsx
@@ -4,7 +4,7 @@ import './FireDots.css';
 const FireDots = () => {
     const containerRef = useRef(null);
     const createdStylesRef = useRef([]); // Keep track of created style elements
-    const particleNum = 15;
+    const particleNum = 25;
     const particleBaseSize = 18;
 
     useEffect(() => {
